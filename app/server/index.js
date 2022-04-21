1705165b2bd691eb0fe9c0b0824d4a8f65f2cb27
ArtIn4rm <kuftem228@gmail.com>
2022-04-21 07:15:54 +0300

    backend begining

---
+++ app/server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const model = require('./models/model')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const routes = require('./routes/router')
+const errorHandler = require('./middleware/errorHandlingMd')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', routes)
+
+app.use(errorHandler)
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server run at port: ${PORT}`))
+    }catch(e){
+        console.log(e)
+    }
+}
+
+ start()
